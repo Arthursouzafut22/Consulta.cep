6514a9351295c1084c43506a9340ff0ce2f0f2d8
Arthursouzafut22 <arthursouzafut22@gmail.com>
2023-12-29 10:52:30 -0300

    Arquivos adicionados

---
+++ mobile.js
@@ -0,0 +1,40 @@
+
+
+
+const inputCep = document.getElementById('cep')
+const btnCep = document.getElementById('btnCep');
+const resultadoCep = document.querySelector('.resultadoCep')
+
+btnCep.addEventListener('click', handleClick)
+
+function handleClick (event) {
+    event.preventDefault();
+    const cep = inputCep.value;
+    buscarCep(cep)
+
+}
+
+function buscarCep(cep) {
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => response.text())
+    .then(body => {
+        resultadoCep.innerText = body;
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
